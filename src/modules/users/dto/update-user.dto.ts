3fd093df67230b87966d1ef31b61ce7abb3d789b
Neil <johnneilmorales@gmail.com>
2023-12-07 16:29:39 +0800

    user bug fix

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,4 +1,5 @@
-import { IsEmail, IsOptional, IsString } from "class-validator";
+import { Transform } from "class-transformer";
+import { IsEmail, IsIn, IsOptional, IsString } from "class-validator";
 
 export class UpdateUserDTO {
   @IsOptional()
@@ -9,4 +10,9 @@ export class UpdateUserDTO {
   @IsString()
   @IsEmail()
   email: string;
+
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value))
+  @IsIn([0, 1]) // 0 - delete, 1 - active
+  status: number;
 }
