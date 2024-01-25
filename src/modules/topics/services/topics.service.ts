6273b7c6adb39116b1cb725732e60994a341b6b6
Neil <johnneilmorales@gmail.com>
2024-01-25 16:33:08 +0800

    topics

--- src/modules/topics/services/topics.service.ts
+++ src/modules/topics/services/topics.service.ts
@@ -3,27 +3,38 @@ import { TopicsRepository } from '../repositories/topics.repository';
 
 @Injectable()
 export class TopicsService {
-    constructor(
-        private readonly topicsRepository: TopicsRepository
-      ) {}
+  constructor(private readonly topicsRepository: TopicsRepository) {}
 
-      async getTopic(id:number) {
-        return this.topicsRepository.findById(id);
-      }
+  async getTopic(id: number) {
+    return this.topicsRepository.findById(id);
+  }
 
-      async getByModuleId(id:number) {
-        return this.topicsRepository.findByModuleId(id);
-      }
+  async getByModuleId(id: number) {
+    return this.topicsRepository.findByModuleId(id);
+  }
 
-      async getTopics() {
-        return this.topicsRepository.find();
-      }
-  
-      async createTopic(data){
-          return this.topicsRepository.insert(data);
-      }
-  
-      async updateTopic(id: number, data) {
-        return this.topicsRepository.updateTopic(id, data);
-      }
+  async getTopics() {
+    return this.topicsRepository.find();
+  }
+
+  async createTopic(data) {
+    return this.topicsRepository.insert(data);
+  }
+
+  async updateTopic(id: number, data) {
+    const topicData = {
+      ...data,
+    };
+
+    if (data.featured_lecture === true && data.library_position <= 0) {
+      const topic = await this.topicsRepository.findById(id);
+      const libraryPosition = await this.topicsRepository.libraryPosition(topic.module_id);
+      const result = libraryPosition as unknown as { _max: { library_position: number } };
+      const highestPosition = result._max.library_position + 1
+
+      topicData.library_position = highestPosition
+    }
+
+    return this.topicsRepository.updateTopic(id, topicData);
+  }
 }
