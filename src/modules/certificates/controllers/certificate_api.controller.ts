6b98901cfeb1870155fc7235909e881b729f2357
Neil <johnneilmorales@gmail.com>
2024-03-25 17:09:28 +0800

    certificate layout

--- src/modules/certificates/controllers/certificate_api.controller.ts
+++ src/modules/certificates/controllers/certificate_api.controller.ts
@@ -30,39 +30,55 @@ export class CertificateApiController {
     const course = await this.coursesService.getCourse(studCertificate.course_id);
     const courseName = course.name.split('+')[0];
 
-    console.log(result)
-
     // if(certificate && result.student_modules.length < 12) return res.status(HttpStatus.BAD_REQUEST).json({ message: 'Template not available. / Not yet complete the course.' });
 
     let endDate;
     let startDate = null;
+    const modules = result.student_modules.length
+    let lectures = 0;
     for (let i = 0; i < result.student_modules.length; i++) {
       if (i > 11) break;
       const module = result.student_modules[i];
-      
-      let speakerName : string;
-      const topics = module.topics as unknown as any
-      topics.forEach(topic => {
-        if(topic.main_topic == 1) speakerName = topic.speaker.name
+
+      let speakerName: string;
+      const topics = module.topics as unknown as any;
+      lectures += topics.length;
+      // topics.forEach(topic => {
+      //   if(topic.main_topic == 1) speakerName = topic.speaker.name
+      // });
+      // moduleHtml += `<div class="child"> ${speakerName ? `<b>${speakerName}</b> - ` : '' } ${module.name}.</div>`;
+      let speakersIds = [];
+
+      topics.forEach((topic) => {
+        if (!speakersIds.includes(topic.speaker.id)) {
+          moduleHtml += `<div class="child">${topic.speaker.name}.</div>`;
+        }
       });
-
-      moduleHtml += `<div class="child"> ${speakerName ? `<b>${speakerName}</b> - ` : '' } ${module.name}.</div>`;
-
-      if (startDate == null) module.start_date;
+      
+      if (startDate == null) startDate = module.start_date;
       endDate = module.start_date;
     }
+    const hours = lectures * 1.5;
 
-    // format end date
-    const newEndDate = new Date(endDate);
-    const endMonth = endDate.getMonth() + 1; // Add 1 because getMonth() returns zero-based index
-    const endYear = newEndDate.getFullYear() % 100; // Get last two digits of the year
-    const formattedEndDate = `${endMonth.toString().padStart(2, '0')}.${endYear.toString().padStart(2, '0')}`;
+    // // format end date
+    // const newEndDate = new Date(endDate);
+    // const endMonth = endDate.getMonth() + 1; // Add 1 because getMonth() returns zero-based index
+    // const endYear = newEndDate.getFullYear() % 100; // Get last two digits of the year
+    // const formattedEndDate = `${endMonth.toString().padStart(2, '0')}.${endYear.toString().padStart(2, '0')}`;
+
+    // // format start date
+    // const newStartDate = new Date(startDate);
+    // const startMonth = endDate.getMonth() + 1; // Add 1 because getMonth() returns zero-based index
+    // const startYear = newStartDate.getFullYear() % 100; // Get last two digits of the year
+    // const formattedStartDate = `${startMonth.toString().padStart(2, '0')}.${startYear.toString().padStart(2, '0')}`;
 
     // format start date
-    const newStartDate = new Date(startDate);
-    const startMonth = endDate.getMonth() + 1; // Add 1 because getMonth() returns zero-based index
-    const startYear = newStartDate.getFullYear() % 100; // Get last two digits of the year
-    const formattedStartDate = `${startMonth.toString().padStart(2, '0')}.${startYear.toString().padStart(2, '0')}`;
+    const newStartDate = this.formatDate(new Date(startDate));
+    // format end date
+    const newEndDate = this.formatDate(new Date(endDate));
+  
+    const completionInfo = `Completed the ${course.name} at NEXT MBA by Attending 12 modules ( ${lectures} lectures, ${hours} hours) and participating in the required assignments during the period between ${newStartDate} and ${newEndDate}.`;
+    const attendanceInfo = `Attended the ${course.name} ( ${modules} modules/ ${lectures} lectures/ ${hours} hours) during period between ${newStartDate} and ${newEndDate}.`
 
     let fontSize: string;
     if (studentInfo.name.length <= 24) fontSize = '60px';
@@ -77,7 +93,8 @@ export class CertificateApiController {
       modules: moduleHtml,
       template: certificate.template,
       certificate_id: studCertificate.certificate_code,
-      certificate_date: `${formattedStartDate} - ${formattedEndDate}`,
+      certificate_date: newEndDate,
+      info: studCertificate.certificate_tier == 1 ? completionInfo : attendanceInfo,
     };
 
     const pdfBuffer = await this.pdfService.certificateGeneratePdf(htmlFilePath, data);
@@ -86,5 +103,12 @@ export class CertificateApiController {
     res.setHeader('Content-Disposition', 'attachment; filename=certificate.pdf');
 
     res.send(pdfBuffer);
+  }
+
+  formatDate(date) {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Add 1 because getMonth() returns zero-based index
+    const year = date.getFullYear();
+    return `${day}.${month}.${year}`;
   }
 }
