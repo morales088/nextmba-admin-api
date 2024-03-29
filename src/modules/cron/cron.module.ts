779c8de617809e5cc4f3d1c58bf98b6883c4d2e2
Christ John Mercurio <christjohn.mercurio04@gmail.com>
2024-03-29 15:01:22 +0800

    course tier cron job

--- src/modules/cron/cron.module.ts
+++ src/modules/cron/cron.module.ts
@@ -5,11 +5,12 @@ import { MailerliteCronService } from './services/mailerlite-cron.service';
 import { StudentsModule } from '../students/students.module';
 import { CoursesModule } from '../courses/courses.module';
 import { SubscriberGroupsModule } from '../subscriber_groups/subscriber-groups.module';
+import { CourseTierCronService } from './services/course_tier-cron.service';
 
 @Module({
   imports: [StudentsModule, CoursesModule, SubscriberGroupsModule],
   controllers: [CronController],
-  providers: [CronService, MailerliteCronService],
+  providers: [CronService, MailerliteCronService, CourseTierCronService],
   exports: [CronService]
 })
 export class CronModule {}
