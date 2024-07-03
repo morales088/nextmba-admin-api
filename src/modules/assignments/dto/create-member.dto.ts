52d4acab658eb4cec1a647c3a3c3e521bc19eeb8
Neil <johnneilmorales@gmail.com>
2024-07-03 18:33:02 +0800

    member fetch

--- src/modules/assignments/dto/create-member.dto.ts
+++ src/modules/assignments/dto/create-member.dto.ts
@@ -2,15 +2,15 @@ import { Transform } from 'class-transformer';
 import { IsBoolean, IsIn, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateMemberDto {
-  // @IsNotEmpty()
-  // @Transform(({ value }) => parseInt(value))
-  // group_id: number;
+  @IsNotEmpty()
+  @Transform(({ value }) => parseInt(value))
+  group_id: number;
 
   @IsNotEmpty()
   @IsString()
   email: string;
 
-  @IsNotEmpty()
-  @Transform(({ value }) => parseInt(value))
-  student_id: number;
+  // @IsNotEmpty()
+  // @Transform(({ value }) => parseInt(value))
+  // student_id: number;
 }
