0f49dad971ea46740d988d6b5be949a7ac8832bf
Christ John Mercurio <christjohn.mercurio04@gmail.com>
2024-08-09 21:19:26 +0800

    refactor: add name and remove logs

--- src/modules/webhooks/webhook.service.ts
+++ src/modules/webhooks/webhook.service.ts
@@ -9,24 +9,21 @@ export class WebhookService {
   async handleEvent(event: Stripe.Event) {
     if (event.type === 'checkout.session.completed') {
       const session = event.data.object as Stripe.Checkout.Session;
-      console.log('💡 ~ session:', session);
       await this.handleSuccessfulPayment(session);
     }
   }
 
   async handleSuccessfulPayment(session: Stripe.Checkout.Session) {
     const metaData = session.metadata;
-    console.log("💡 ~ metaData:", metaData)
+    console.log('💡 ~ metaData:', metaData);
 
     const paymentData = {
-      name: metaData.email,
+      name: metaData.name,
       email: metaData.email,
       product_code: metaData.product_code,
       price: metaData.price,
     };
-    console.log("💡 ~ paymentData:", paymentData)
 
-    const payment = await this.paymentService.createPayment(paymentData);
-    console.log("💡 ~ payment:", payment)
+    return this.paymentService.createPayment(paymentData);
   }
 }
