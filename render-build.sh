59c2c773e58f80b3008afbd483dfd43dfba1ea57
Neil <johnneilmorales@gmail.com>
2024-09-07 14:30:24 +0800

    render

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,5 @@
-npm install;
-npx prisma db push;
-npx prisma generate;
-nest build;
 npx puppeteer browsers install chrome;
+# npm install;
+# npx prisma db push;
+# npx prisma generate;
+# nest build;
