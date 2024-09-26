5ec84bc1e11bda457348009a0f6a11af84a584f5
Gerald Tolentino <geraldtolentino82@yahoo.com.ph>
2024-09-26 20:48:13 +0800

    Added features to aid course tracking

--- src/modules/student-plan/student-plan.controller.ts
+++ src/modules/student-plan/student-plan.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Param, Patch, Post } from '@nestjs/common';
+import { Controller, Param, Patch, Post, Query } from '@nestjs/common';
 import { StudentPlanService } from './services/student-plan.service';
 
 @Controller('student-plan')
@@ -15,8 +15,23 @@ export class StudentPlanController {
     return await this.studentPlanService.activatePremium(studentId);
   }
 
+  @Patch('/renew-premium/:studentId')
+  async renewPremium(@Param('studentId') studentId: number) {
+    return await this.studentPlanService.renewPremium(studentId);
+  }
+
   @Patch('/end-premium/:studentId')
   async endPremium(@Param('studentId') studentId: number) {
     return await this.studentPlanService.endPremium(studentId);
+  }
+
+  @Patch('/convert-basic/:studentId')
+  async convertBasic(@Param('studentId') studentId: number, @Query('studentCourseId') studentCourseId: number) {
+    return await this.studentPlanService.convertBasic(studentId, studentCourseId);
+  }
+
+  @Patch('/add-premium-courses/:studentId')
+  async addNewPremiumCourses(@Param('studentId') studentId: number) {
+    return await this.studentPlanService.addNewPremiumCourses(studentId);
   }
 }
