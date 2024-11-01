64363c8ccad9f69a29cc2d267314b60112bd8c00
Christ John Mercurio <christjohn.mercurio04@gmail.com>
2024-11-02 00:54:47 +0800

    chore: remove abstract repository in affiliates

--- src/modules/affiliates/repositories/affiliate.repository.ts
+++ src/modules/affiliates/repositories/affiliate.repository.ts
@@ -1,14 +1,11 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
-import { AbstractRepository } from 'src/common/repositories/abstract.repository';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/common/prisma/prisma.service';
 import { UpdateAffiliateDto } from '../dto/update-affiliate.dto';
 import { Affiliates } from '@prisma/client';
 
 @Injectable()
-export class AffiliateRepository extends AbstractRepository<Affiliates> {
-  constructor(protected readonly prisma: PrismaService) {
-    super(prisma);
-  }
+export class AffiliateRepository {
+  constructor(protected readonly database: PrismaService) {}
 
   get modelName(): string {
     return 'Affiliates'; // Specify the Prisma model name for entity
@@ -17,7 +14,7 @@ export class AffiliateRepository extends AbstractRepository<Affiliates> {
   async find(pageNumber: number, perPage: number): Promise<Affiliates[]> {
     const skipAmount = (pageNumber - 1) * perPage;
 
-    return this.prisma.affiliates.findMany({
+    return this.database.affiliates.findMany({
       include: { student: true },
       skip: skipAmount,
       take: perPage,
@@ -26,7 +23,7 @@ export class AffiliateRepository extends AbstractRepository<Affiliates> {
   }
 
   async findById(id: number) {
-    return this.prisma.affiliates.findFirst({
+    return this.database.affiliates.findFirst({
       include: { student: true },
       where: { id },
     });
@@ -37,7 +34,7 @@ export class AffiliateRepository extends AbstractRepository<Affiliates> {
 
     if (!affiliate) throw new NotFoundException('Affiliate does not exist.');
 
-    return this.prisma.affiliates.update({
+    return this.database.affiliates.update({
       where: { id },
       data: updateData,
       include: { student: true },
